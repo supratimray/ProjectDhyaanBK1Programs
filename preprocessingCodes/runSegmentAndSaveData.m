d1048ea093db50f028858a17ba225b1adb0b2308
Ankan Biswas <ankanbiswas022@gmail.com>
2023-11-22 09:41:07 +0530

    1. Adding 'BK1AllSubjectList.mat' which includes all the subjects before pairing. 2. Updated the 'runSegmentAndSaveData' to extract data for all the subjects/    The subjects which has issues during extraction are also listed. 3. Minor update to the 'segmentAndSaveData' function to process eye data for the trials which has 'time' issues.

--- preprocessingCodes/runSegmentAndSaveData.m
+++ preprocessingCodes/runSegmentAndSaveData.m
@@ -1,27 +1,53 @@
-% runSegmentAndSaveData
-pairedSubjectNameList = getMatchedSubjectsBK1;
-[subjectNameList,expDateList] = getDemographicDetails('BK1');
+% runSegmentAndSaveData for the BK1 project
 
-goodSubjectList = pairedSubjectNameList(:);
-
-folderSourceString = 'D:\OneDrive - Indian Institute of Science\Supratim\Projects\ProjectDhyaan\BK1';
-
-segmentTheseIndices = 13:length(goodSubjectList);
-gridType = 'EEG'; 
+gridType = 'EEG';
 capType = 'actiCap64_2019';
-impedanceTag = '_Impedance_Start'; 
+impedanceTag = '_Impedance_Start';
 displayFlag = 0;
 FsEye = 1000;
+% folderSourceString = 'D:\OneDrive - Indian Institute of Science\Supratim\Projects\ProjectDhyaan\BK1';
+folderSourceString = 'D:\Projects\ProjectDhyaan\BK1';
+[subjectNameList,expDateList] = getDemographicDetails('BK1');
 
-for i=1:length(segmentTheseIndices)
-    subjectName = goodSubjectList{segmentTheseIndices(i)};
+% get the paired subjectList
+% pairedSubjectNameList = getMatchedSubjectsBK1;
+% goodSubjectList = pairedSubjectNameList(:);
+
+% get all the subjectList
+fileToLoadSourceString = 'D:\Projects\ProjectDhyaan\BK1\ProjectDhyaanBK1Programs\commonAnalysisCodes\informationFiles';
+fileName = 'BK1AllSubjectList.mat';
+load(fullfile(fileToLoadSourceString,fileName),'allSubjectList');
+
+allIndices = 1:length(allSubjectList);
+problamaticIndices  = [5 12 75]; % this indices need to be extracted separately 
+segmentTheseIndices  = setdiff(allIndices,problamaticIndices);
+
+for i=segmentTheseIndices
+    subjectName = allSubjectList{i};
     disp(['Extracting Subject ' subjectName]);
-    
+    %-------------------------------------------------------------------------------------------------------------------------------
     expDate = expDateList{strcmp(subjectName,subjectNameList)};
     segmentAndSaveData(subjectName,expDate,folderSourceString,FsEye); % Segment data
     getImpedanceDataEEG(subjectName,expDate,folderSourceString,gridType,impedanceTag,displayFlag,capType); % Get Impedance
 end
 
-% Errors found in
-% i=5; 095KM
-% i=12; 053DR
+%-----------------------------------------------------------------------------------------------------
+% Issues during the data segment process:
+% problamaticIndices  = [5 12 75];
+% goodSegmentIndices  = setdiff(segmentTheseIndices,problamaticIndices);
+% ----------------------------------------------------------------------------------------------------
+% Marker Issues:-
+% i=5;  095KM, M2 protocol, .vmrk file had junk markers; for extraction we have used the markers which matches with the .bhv2 file;
+% i=75; 099SP, M2 protocol has less trial start markers compared to .bhv2 file. Saved data for only the trials which have markers matched between
+%       .vmrk and .bhv2 file. 2 trials were lost at the beggining of the experiment
+% ----------------------------------------------------------------------------------------------------
+% File Missing:-
+% i=12; 053DR, EO1 protocol could not be extracted as .bhv2 file is missing;
+%-----------------------------------------------------------------------------------------------------
+% Time issues:- (could be extracted with the updated version of the segmentation code)
+% Fixed with additional condition while processing the eye data in the segmentation codes
+% i=15; 035SS, EC2
+% i=26; 006SR, EO1 protocol
+% i=35; 089AB, EO1 and EC1 protocol
+% i=50; 064PK, EO1 and EC1 protocol
+%-----------------------------------------------------------------------------------------------------
--- preprocessingCodes/segmentAndSaveData.m
+++ preprocessingCodes/segmentAndSaveData.m
@@ -7,13 +7,13 @@
 % There are 8 protocols as listed below.
 % 1. EO1 (eyes open 1): Duration: 5 minutes.
 % 2. EC1 (eyes close 1): Duration: 5 minutes
-% 3. G1  (Gamma 1): Duration: 5 minutes. 
+% 3. G1  (Gamma 1): Duration: 5 minutes.
 % 4. M1  (Meditation 1): Duration: 15 minutes.
 % 5. G2 (Gamma 2): Same as G1
 % 6. EO2 (eyes open 2): Same as EO1
 % 7. EC2 (eyes close 2): Same as EC2
 % 8. M2 (Meditation 2): Duration: 15 minutes - same as M1 but with gamma
-% protocol running 
+% protocol running
 
 function segmentAndSaveData(subjectName,expDate,folderSourceString,FsEye)
 
@@ -28,75 +28,74 @@ gridType = 'EEG';
 protocolNameList = [{'EO1'} {'EC1'} {'G1'} {'M1'} {'G2'} {'EO2'} {'EC2'} {'M2'}];
 
 timeStartFromBaseLine = -1.25; deltaT = 2.5;
-
 trialStartCode = 9; trialEndCode = 18;
 
 for i=1:length(protocolNameList)
     protocolName = protocolNameList{i};
-    
+
     % Get Digital events from BrainProducts (BP)
     folderName = fullfile(folderSourceString,'data',subjectName,gridType,expDate,protocolName);
     folderExtract = fullfile(folderName,'extractedData');
     folderSave = fullfile(folderName,'segmentedData','eyeData');
-    
+
     [digitalTimeStamps,digitalEvents]=extractDigitalDataBrainProducts(subjectName,expDate,protocolName,folderSourceString,gridType,0);
     trialStartTimesListBP = digitalTimeStamps(digitalEvents==trialStartCode);
     trialEndTimesListBP = digitalTimeStamps(digitalEvents==trialEndCode);
-    
+
     % Get data from ML
     fileNameML = fullfile(folderSourceString,'data','rawData',[subjectName expDate],[subjectName expDate protocolName '.bhv2']);
     [MLData,MLConfig,MLTrialRecord] = mlread(fileNameML);
-    
+
     numTrials = length(MLData);
     if numTrials ~= length(trialStartTimesListBP)
         error('Number of trials in ML do not match with number of start trial codes in BP');
     end
-    
+
     goodStimTimes = []; % This is a long sequence of epoch times around which data segments are cut
     goodStimTimesML = []; % Same sequence constructed using ML data. Used for comparison
     goodStimCodeNums = [];
-    
+
     eyeRawData = [];
     eyeRawGazeData = [];
     eyeDataDegX = [];
-    eyeDataDegY = [];    
-    
+    eyeDataDegY = [];
+
     for j=1:numTrials
         trialStartTimeBP = trialStartTimesListBP(j); % Start of trial as per BP timeline
         trialEndTimeBP = trialEndTimesListBP(j); % End of trial as per BP timeline
         MLDataThisTrial = MLData(j);
-        
+
         if strcmp(protocolName(1),'E') || strcmp(protocolName,'M1') % for EO, EC and M1 protocols
 
             trialStartTimeML = MLDataThisTrial.BehavioralCodes.CodeTimes((MLDataThisTrial.BehavioralCodes.CodeNumbers==trialStartCode)); % in milliseconds
             goodStimTimeThisTrial = (trialStartTimeML/1000) - timeStartFromBaseLine; % Relative to this trial (in seconds)
-            
+
             goodStimTimes = cat(2,goodStimTimes,trialStartTimeBP + goodStimTimeThisTrial); % in seconds
             goodStimTimesML = cat(2,goodStimTimesML,MLDataThisTrial.AbsoluteTrialStartTime/1000 + goodStimTimeThisTrial); % in seconds
-            
+
         elseif strcmp(protocolName(1),'G') || strcmp(protocolName,'M2') % for G1, G2 and M2 protocols
 
             % ML
             codeTimesThisTrialML = MLDataThisTrial.BehavioralCodes.CodeTimes;
             codesNumbersThisTrialML = MLDataThisTrial.BehavioralCodes.CodeNumbers;
-            
+
             % BP
             eventPosThisTrial = intersect(find(digitalTimeStamps>=trialStartTimeBP),find(digitalTimeStamps<=trialEndTimeBP));
             digitalTimeStampsThisTrial = digitalTimeStamps(eventPosThisTrial);
             digitalEventsThisTrial = digitalEvents(eventPosThisTrial);
-            
+
             % The events should match
             if ~isequal(digitalEventsThisTrial(:),codesNumbersThisTrialML(:))
                 disp('Code numbers do not match');
             end
-            
+
             goodStimPosThisTrial = getGoodStimPosGammaProtocol(digitalEventsThisTrial);
             goodStimTimeThisTrial = codeTimesThisTrialML(goodStimPosThisTrial)/1000; % Relative to this trial (in seconds)
-            
+
             goodStimTimes = cat(2,goodStimTimes,digitalTimeStampsThisTrial(goodStimPosThisTrial)); % in seconds, directly from the BP digital stream
             goodStimTimesML = cat(2,goodStimTimesML,MLDataThisTrial.AbsoluteTrialStartTime/1000 + goodStimTimeThisTrial');
         end
-        
+
         % Get and Align eye data (RawData)
         eyeRawDataThisTrial = getEyeDataThisTrial(MLDataThisTrial.AnalogData.Eye,goodStimTimeThisTrial,timeStartFromBaseLine,deltaT,FsEye);
         eyeRawData = cat(1,eyeRawData,eyeRawDataThisTrial);
@@ -107,10 +106,10 @@ for i=1:length(protocolNameList)
         [eyeDataDegXThisTrial,eyeDataDegYThisTrial] = convertEyeDataPix2DegML(squeeze(eyeGazeDataThisTrial));
         eyeDataDegX =  cat(1,eyeDataDegX,eyeDataDegXThisTrial'); eyeDataDegY = cat(1,eyeDataDegY,eyeDataDegYThisTrial');
     end
-    
+
     compareBPWithML(goodStimTimes,goodStimTimesML);
-    eyeRangeMS = (timeStartFromBaseLine + [0 deltaT])*1000;     
-    
+    eyeRangeMS = (timeStartFromBaseLine + [0 deltaT])*1000;
+
     % Save useful data in folderExtract
     makeDirectory(folderExtract);
     save(fullfile(folderExtract,'goodStimCodeNums.mat'),'goodStimCodeNums','goodStimTimes');
@@ -120,9 +119,10 @@ for i=1:length(protocolNameList)
     getEEGDataBrainProducts(subjectName,expDate,protocolName,folderSourceString,gridType,goodStimTimes,timeStartFromBaseLine,deltaT);
     % Save eye data in segmentedData(ML)
     makeDirectory(folderSave);
-    save(fullfile(folderSave,'eyeDataDeg.mat'),'eyeDataDegX','eyeDataDegY');    
+    save(fullfile(folderSave,'eyeDataDeg.mat'),'eyeDataDegX','eyeDataDegY');
 end
 end
+
 function goodStimPos = getGoodStimPosGammaProtocol(digitalEvents)
 goodCodeNums = 21:28; % Stim codes
 
@@ -139,7 +139,7 @@ dTBP = diff(goodStimTimes);
 dTML = diff(goodStimTimesML);
 maxD = 1000*max(abs(dTBP-dTML));
 
-maxTol = 25; % Must be within this limit
+maxTol = 35; % Must be within this limit
 if maxD>maxTol
     plot(dTBP,'b'); hold on; plot(dTML,'r');
     error(['max difference in timing: ' num2str(maxD) 'exceeds maxTolerence of ' num2str(maxTol) ' ms']);
@@ -154,9 +154,15 @@ numSamplesToUse = round(epochDur*FsEye);
 xsEye = (1:maxEyePos)/FsEye;
 
 for k=1:length(goodStimTimesThisTrial)
-    startPos = find(xsEye<(goodStimTimesThisTrial(k)+timeStartFromBaseLine), 1, 'last' );
+    trialstartTime = goodStimTimesThisTrial(k)+timeStartFromBaseLine;
+    if trialstartTime < xsEye(1)
+        startPos = 1;
+    else
+        startPos = find(xsEye<trialstartTime, 1, 'last' );
+    end
+
     if (startPos+numSamplesToUse)<=maxEyePos
-        eyeDataThisTrial(k,:,:) = eyeDataML(startPos + (1:numSamplesToUse),:); %#ok<*AGROW> 
+        eyeDataThisTrial(k,:,:) = eyeDataML(startPos + (1:numSamplesToUse),:); %#ok<*AGROW>
     else
         disp('Trial aborted before time... ');
         posShort = (startPos+1):maxEyePos;
