0a2e759a0838e1acf5ff5278bca9bc5b5d2899c6
Ankan Biswas <ankanbiswas022@gmail.com>
2023-11-28 09:31:02 +0530

    Adding getDeclaredBadElec function and minor changes in other functions for added display functionalities.

---
+++ commonAnalysisCodes/informationFiles/getDeclaredBadElecs.m
@@ -0,0 +1,11 @@
+function [declaredBadElectrodes] = getDeclaredBadElecs(threshold)
+% Gives the declared bad electrodelist according to the current threshold
+% We get the this list after setting the desired thresold in the runDisplayBadElecsAcrossSubjectsBK1 script
+
+if ~exist('threshold','var');   threshold=0.35;       end
+
+if threshold==0.35
+    declaredBadElectrodes = [1  28  29  38  39  55  57  58]; % threshold 0.35
+end
+end
+
