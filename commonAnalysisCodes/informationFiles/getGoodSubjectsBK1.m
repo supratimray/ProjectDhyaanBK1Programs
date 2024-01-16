e8da53664b596bb2512f37b14052f75d8c000a8c
Ankan Biswas <ankanbiswas022@gmail.com>
2024-01-16 08:09:04 +0530

    Minor changes to the subject information files to remove bad subjects

--- commonAnalysisCodes/informationFiles/getGoodSubjectsBK1.m
+++ commonAnalysisCodes/informationFiles/getGoodSubjectsBK1.m
@@ -1,6 +1,6 @@
 function [allSubjectList, meditatorList, controlList] = getGoodSubjectsBK1
 
-declaredBadSubjects = [{'004P'} {'081SN'}];
+declaredBadSubjects = [{'004P'} {'081SN'} {'069MG'} {'092KB'}]; % More than 45 percent electrodes are bad
 tmp = load('BK1AllSubjectList.mat');
 allSubjectList = setdiff(tmp.allSubjectList,declaredBadSubjects,'stable');
 meditatorList = setdiff(tmp.meditatorList,declaredBadSubjects,'stable');
